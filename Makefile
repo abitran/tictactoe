3a7b1aeb0ae1762f0af79a61b37a87e79b7ee043
Alejandro Bitran <abitran@gmail.com>
2024-03-18 17:05:56 +0200

    optimizing

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-CFLAGS=-Wall -ggdb -std=c11 -pedantic `pkg-config --cflags sdl2`
-LIBS=`pkg-config --libs sdl2`
+CFLAGS=-Wall -ggdb -std=c11 -pedantic `pkg-config --cflags sdl2 SDL2_gfx` 
+LIBS=`pkg-config sdl2 SDL2_gfx --libs`
 
 tictactoe: tictactoe.c 
 	$(CC) $(CFLAGS) -o tictactoe tictactoe.c $(LIBS)
--- tictactoe.c
+++ tictactoe.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #include <stdio.h>
 
 #define S_WIDTH 800
@@ -11,45 +12,93 @@
 #define CELLW 266
 #define CELLH 200
 
+typedef enum{
+  RUNNING=0,  
+  PLAYER_X_WON, 
+  PLAYER_O_WON, 
+  TIED, 
+  QUIT 
+}State;
+
+typedef enum {
+  EMPTY=0,
+  PLAYER_X,
+  PLAYER_O
+}Player;
+
+typedef struct {
+  int grid[ROWS * COLS];
+  Player player;
+  State state;
+}Game;
+
 int sdlchk(int);
 
 void *sdlpt(void *);
 
-void render_grid(SDL_Renderer *);
+void render_game(SDL_Renderer *, const Game *);
+
+void selectsq(Game *, int, int);
+
+void render_running(SDL_Renderer *, const Game *);
+
+void render_grid(SDL_Renderer *, const int *, 
+                 const SDL_Color *, const SDL_Color *);
+
+
+void render_x(SDL_Renderer *, int, int, const SDL_Color *);
+void render_o(SDL_Renderer *, int, int, const SDL_Color *);
+
+const SDL_Color PLAYER_X_COLOR = { .r=255, .g=0, .b=0};
+const SDL_Color PLAYER_O_COLOR = { .r=0, .g=0, .b=255};
 
 int main(int argc, char **argv) {
+ 
+  // Initial state of the game.
+  Game game = {
+      .grid = {PLAYER_X, EMPTY, EMPTY, 
+               EMPTY, EMPTY, EMPTY, 
+              EMPTY, EMPTY, EMPTY},
+      .player=PLAYER_X,
+      .state=RUNNING
+  };
+
   sdlchk((SDL_Init(SDL_INIT_VIDEO)));
 
   SDL_Window *window =
       sdlpt(SDL_CreateWindow("Tictactoe", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, S_WIDTH, S_HEIGHT, 0));
+                       SDL_WINDOWPOS_CENTERED, S_WIDTH, S_HEIGHT, SDL_WINDOW_SHOWN));
 
   Uint32 render_flags = SDL_RENDERER_ACCELERATED;
 
   SDL_Renderer *renderer = sdlpt(SDL_CreateRenderer(window, -1, render_flags));
 
-  int quit = 0;
-  while (!quit) {
+  while (game.state != QUIT) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
         case SDL_QUIT:
-          quit = 1;
-        break;
+          game.state = QUIT;
+          break;
+
+        case SDL_MOUSEBUTTONDOWN:
+          selectsq(&game, event.button.x / CELLW, event.button.y / CELLH);
+          break;
       }
     }
 
     sdlchk(SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255));
     sdlchk(SDL_RenderClear(renderer));
-
-    render_grid(renderer);
-
+    render_game(renderer, &game);
     SDL_RenderPresent(renderer);
 
   }
 
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
-  return 0;
+
+  return EXIT_SUCCESS;
 }
 
 int sdlchk(int code) {
@@ -69,9 +118,12 @@ void *sdlpt(void *ptr) {
   return ptr;
 }
 
-void render_grid(SDL_Renderer *renderer) {
+void render_grid(SDL_Renderer *renderer, 
+                 const int *grid, 
+                 const SDL_Color *player_x_color, 
+                 const SDL_Color *player_o_color) {
 
-  sdlchk(SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255));
+  sdlchk(SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255));
   for (int x = 0; x < ROWS; x++) {
     sdlchk(SDL_RenderDrawLine(renderer, 
                        x * CELLW, 
@@ -86,7 +138,70 @@ void render_grid(SDL_Renderer *renderer) {
                               S_WIDTH, 
                               y * CELLH));
   }
+
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      switch (grid[i*ROWS + j]) {
+        case PLAYER_X:
+          render_x(renderer, i, j, player_x_color);
+          break;
+
+        case PLAYER_O:
+          render_o(renderer, i, j, player_o_color);
+          break;
+        default:{}
+        
+      }
+    }
+  }
 }
 
+void render_game(SDL_Renderer *renderer, const Game *game) {
+
+  switch (game->state) {
+    case RUNNING:
+      render_running(renderer, game);
+      break;
+    case PLAYER_O_WON:
+      break;
+    case PLAYER_X_WON:
+      break;
+    case TIED:
+      break;
+    default:
+      break;
+  }
+
+}
+
+void render_running(SDL_Renderer *renderer, const Game *game) {
+
+  render_grid(renderer, game->grid, &PLAYER_O_COLOR, 
+              &PLAYER_X_COLOR);
+  
+}
+
+void selectsq(Game *game, int row, int col) {
+
+
+}
+
+void render_x(SDL_Renderer *renderer, int row, int col, const SDL_Color *color) {
+
+  const float hatzi_box = fmin(CELLW, CELLH) * 0.25;
+  const float center_x = CELLW * 0.5 * col * CELLW;
+  const float center_y = CELLH * 0.5 * row * CELLH;
+  thickLineRGBA(renderer, 
+                center_x - hatzi_box, 
+                center_y - hatzi_box, 
+                center_x + hatzi_box, 
+                center_y + hatzi_box, 
+                10, color->r, color->g, 
+                color->b, 255);
+}
+
+void render_o(SDL_Renderer *renderer, int row, int col, const SDL_Color *color){
+
+}
 
 
