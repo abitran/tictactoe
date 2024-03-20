afb6fec82c9c85554c98926e8edf1c8c8feff1e7
Alejandro Bitran <abitran@gmail.com>
2024-03-20 10:02:45 +0200

    last edition

--- tictactoe.c
+++ tictactoe.c
@@ -32,22 +32,37 @@ void selectsq(Game *, int, int);
 
 void render_running(SDL_Renderer *, const Game *);
 
+void render_end(SDL_Renderer *, const Game *, const SDL_Color *);
+
 void render_grid(SDL_Renderer *, const int *, const SDL_Color *,
                  const SDL_Color *);
 
 void render_x(SDL_Renderer *, int, int, const SDL_Color *);
 void render_o(SDL_Renderer *, int, int, const SDL_Color *);
 
+void play(Game *, int, int);
+
+void reset(Game *);
+
+void switch_player(Game *);
+
+void game_over(Game *);
+
+int count_cells(const int *, int);
+
+int check_player_won(Game *, int);
+
 const SDL_Color PLAYER_X_COLOR = {.r = 255, .g = 0, .b = 0};
 const SDL_Color PLAYER_O_COLOR = {.r = 0, .g = 0, .b = 255};
+const SDL_Color TIED_COLOR = {.r = 130, .g = 130, .b = 130};
 
 int main(int argc, char **argv) {
 
   // Initial state of the game.
-  Game game = {.grid = {PLAYER_X, EMPTY, EMPTY, EMPTY, PLAYER_O, EMPTY, EMPTY,
-                        EMPTY, PLAYER_X},
-               .player = PLAYER_X,
-               .state = RUNNING};
+  Game game = {
+      .grid = {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+      .player = PLAYER_X,
+      .state = RUNNING};
 
   sdlchk((SDL_Init(SDL_INIT_VIDEO)));
 
@@ -59,8 +74,8 @@ int main(int argc, char **argv) {
 
   SDL_Renderer *renderer = sdlpt(SDL_CreateRenderer(window, -1, render_flags));
 
+  SDL_Event event;
   while (game.state != QUIT) {
-    SDL_Event event;
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
       case SDL_QUIT:
@@ -68,7 +83,7 @@ int main(int argc, char **argv) {
         break;
 
       case SDL_MOUSEBUTTONDOWN:
-        selectsq(&game, event.button.x / CELLW, event.button.y / CELLH);
+        selectsq(&game, event.button.y / CELLW, event.button.x / CELLW);
         break;
       }
     }
@@ -138,10 +153,13 @@ void render_game(SDL_Renderer *renderer, const Game *game) {
     render_running(renderer, game);
     break;
   case PLAYER_O_WON:
+    render_end(renderer, game, &PLAYER_O_COLOR);
     break;
   case PLAYER_X_WON:
+    render_end(renderer, game, &PLAYER_X_COLOR);
     break;
   case TIED:
+    render_end(renderer, game, &TIED_COLOR);
     break;
   default:
     break;
@@ -153,7 +171,86 @@ void render_running(SDL_Renderer *renderer, const Game *game) {
   render_grid(renderer, game->grid, &PLAYER_X_COLOR, &PLAYER_O_COLOR);
 }
 
-void selectsq(Game *game, int row, int col) {}
+void render_end(SDL_Renderer *renderer, const Game *game,
+                const SDL_Color *color) {
+  render_grid(renderer, game->grid, color, color);
+}
+
+void selectsq(Game *game, int row, int col) {
+  if (game->state == RUNNING)
+    play(game, row, col);
+  else
+    reset(game);
+}
+
+void play(Game *game, int row, int col) {
+  if (game->grid[row * ROWS + col] == EMPTY) {
+    game->grid[row * ROWS + col] = game->player;
+    switch_player(game);
+    game_over(game);
+  }
+}
+
+void switch_player(Game *game) {
+  if (game->player == PLAYER_X)
+    game->player = PLAYER_O;
+  else if (game->player == PLAYER_O)
+    game->player = PLAYER_X;
+}
+
+void reset(Game *game) {
+  game->player = PLAYER_X;
+  game->state = RUNNING;
+  for (int i = 0; i < ROWS * COLS; i++)
+    game->grid[i] = EMPTY;
+}
+
+int count_cells(const int *grid, int cell) {
+
+  int counter = 0;
+  for (int i = 0; i < ROWS * COLS; i++) {
+    if (grid[i] == cell)
+      counter++;
+  }
+
+  return counter;
+}
+
+void game_over(Game *game) {
+  if (check_player_won(game, PLAYER_X))
+    game->state = PLAYER_X_WON;
+  else if (check_player_won(game, PLAYER_O))
+    game->state = PLAYER_O_WON;
+  else if (count_cells(game->grid, EMPTY) == 0)
+    game->state = TIED;
+}
+
+int check_player_won(Game *game, int player) {
+  int row_count = 0, col_count = 0;
+  int diag1_count = 0, diag2_count = 0;
+
+  for (int i = 0; i < ROWS; i++) {
+    for (int j = 0; j < COLS; j++) {
+      if (game->grid[i * ROWS + j] == player)
+        row_count++;
+      if (game->grid[j * COLS + i] == player)
+        col_count++;
+    }
+    if (row_count >= ROWS || col_count >= COLS)
+      return 1;
+
+    row_count = 0;
+    col_count = 0;
+
+    if (game->grid[i * ROWS + i] == player)
+      diag1_count++;
+
+    if (game->grid[i * COLS + COLS - i - 1] == player)
+      diag2_count++;
+  }
+
+  return diag1_count >= ROWS || diag2_count >= COLS;
+}
 
 void render_x(SDL_Renderer *renderer, int row, int col,
               const SDL_Color *color) {
@@ -175,12 +272,8 @@ void render_o(SDL_Renderer *renderer, int row, int col,
   const float hatzi_box = fmin(CELLW, CELLH) * 0.4;
   const float center_x = CELLW * 0.5 + col * CELLW;
   const float center_y = CELLH * 0.5 + row * CELLH;
-  filledCircleRGBA(renderer, center_x, center_y, 
-                   hatzi_box, color->r, color->g, 
+  filledCircleRGBA(renderer, center_x, center_y, hatzi_box, color->r, color->g,
                    color->b, 255);
 
-  filledCircleRGBA(renderer, center_x, center_y, 
-                   hatzi_box-12, 0, 0, 0, 255);
-
-
+  filledCircleRGBA(renderer, center_x, center_y, hatzi_box - 12, 0, 0, 0, 255);
 }
